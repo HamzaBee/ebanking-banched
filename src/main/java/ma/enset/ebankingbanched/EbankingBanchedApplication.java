df7620c863da63c7b8ac53a5ebb12a6210c44c15
HamzaBee <hamzabenbrahim6060@gmail.com>
2026-07-06 15:50:48 +0100

    Initial commit

---
+++ src/main/java/ma/enset/ebankingbanched/EbankingBanchedApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.ebankingbanched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingBanchedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingBanchedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/ebankingbanched/EbankingBanchedApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.ebankingbanched;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingBanchedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
